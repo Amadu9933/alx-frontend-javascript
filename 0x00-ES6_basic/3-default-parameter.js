f7744e622f06f210d824d27ed095bfbe76ffd4bb
Amadu <amaduhamzajaarah300@gmail.com>
2023-06-27 01:06:32 +0700

    ES6 method properties

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -3,8 +3,5 @@ export default function getSumOfHoods(
   expansion1989 = 89,
   expansion2019 = 19
 ) {
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
   return initialNumber + expansion1989 + expansion2019;
 }
