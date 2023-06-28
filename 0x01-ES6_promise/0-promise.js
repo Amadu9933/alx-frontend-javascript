e3bba239686771af421403a80ad1a5f58ead3e2a
Amadu <amaduhamzajaarah300@gmail.com>
2023-06-28 16:16:12 +0700

    my promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
-async function getResponseFromAPI() {
-  return Promise.resolve("hamraazAPI.com");
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
-
-export default getResponseFromAPI;
